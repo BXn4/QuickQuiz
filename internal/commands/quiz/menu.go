d4b2fe4084f15c8eff57650912fdac375e032e51
bxn4 <bencefodor@proton.me>
2025-05-02 19:42:34 +0200

    Added some buttons

--- internal/commands/quiz/menu.go
+++ internal/commands/quiz/menu.go
@@ -9,6 +9,8 @@ import (
 
 func HandleQuizMenu(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	languageCode := "en"
+	username := "bence:|NXM"
+	coins := 100
 	level := 10
 	levelProgress := 41.5
 	levelNeededXP := 100
@@ -16,7 +18,8 @@ func HandleQuizMenu(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	streak := 7
 	bestStreak := 14
 
-	titleStr := utils.GetTranslation(languageCode, "MENU_STATS")
+	titleStr := fmt.Sprintf(utils.GetTranslation(languageCode, "MENU_STATS"), username)
+	coinsStr := fmt.Sprintf(utils.GetTranslation(languageCode, "MENU_COINS"), coins)
 	levelStr := fmt.Sprintf(utils.GetTranslation(languageCode, "MENU_LEVEL"), level, int(levelProgress))
 	progressStr := fmt.Sprintf(utils.GetTranslation(languageCode, "MENU_LEVELUP"), levelNeededXP, level+1)
 	correctAnswersStr := fmt.Sprintf(utils.GetTranslation(languageCode, "MENU_CORRECT_ANSWERS"), correctAnswers)
@@ -24,8 +27,9 @@ func HandleQuizMenu(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	bestStreakStr := fmt.Sprintf(utils.GetTranslation(languageCode, "MENU_BEST_STREAK"), bestStreak)
 
 	description := fmt.Sprintf(
-		"%s %s\n\n %s %s\n %s %s\n\n %s %s\n %s %s\n %s %s",
+		"%s %s\n\n %s %s\n %s %s\n %s %s\n\n %s %s\n %s %s\n %s %s",
 		utils.Emojis.STATS, titleStr,
+		utils.Emojis.COINS, coinsStr,
 		utils.Emojis.LEVEL, levelStr,
 		utils.Emojis.PROGRESS, progressStr,
 		utils.Emojis.CORRECT_ANSWERS, correctAnswersStr,
@@ -39,11 +43,59 @@ func HandleQuizMenu(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		Color:       utils.Colors.PrimaryColor,
 	}
 
+	buttons := []discordgo.MessageComponent{
+		discordgo.ActionsRow{
+			Components: []discordgo.MessageComponent{
+				discordgo.Button{
+					Label:    fmt.Sprintf("%s %s", utils.Emojis.QUIZ_NEW, utils.GetTranslation(languageCode, "QUIZ_NEW")),
+					Style:    discordgo.SecondaryButton,
+					CustomID: "quiz_new",
+				},
+
+				discordgo.Button{
+					Label:    fmt.Sprintf("%s %s", utils.Emojis.QUIZ_LIST, utils.GetTranslation(languageCode, "QUIZ_LIST")),
+					Style:    discordgo.SecondaryButton,
+					CustomID: "quiz_list",
+				},
+			},
+		},
+		discordgo.ActionsRow{
+			Components: []discordgo.MessageComponent{
+				discordgo.Button{
+					Label:    fmt.Sprintf("%s %s", utils.Emojis.QUIZ_PACKS, utils.GetTranslation(languageCode, "QUIZ_PACKS")),
+					Style:    discordgo.SecondaryButton,
+					CustomID: "quiz_packs",
+				},
+				discordgo.Button{
+					Label:    fmt.Sprintf("%s %s", utils.Emojis.QUIZ_TOPLIST, utils.GetTranslation(languageCode, "QUIZ_TOPLIST")),
+					Style:    discordgo.SecondaryButton,
+					CustomID: "quiz_toplist",
+				},
+			},
+		},
+		discordgo.ActionsRow{
+			Components: []discordgo.MessageComponent{
+
+				discordgo.Button{
+					Label:    fmt.Sprintf("%s %s", utils.Emojis.QUIZ_SHOP, utils.GetTranslation(languageCode, "QUIZ_SHOP")),
+					Style:    discordgo.SecondaryButton,
+					CustomID: "quiz_shop",
+				},
+				discordgo.Button{
+					Label:    fmt.Sprintf("%s %s", utils.Emojis.QUIZ_SPONSOR, utils.GetTranslation(languageCode, "QUIZ_SPONSOR")),
+					Style:    discordgo.PrimaryButton,
+					CustomID: "quiz_sponsor",
+				},
+			},
+		},
+	}
+
 	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Embeds: []*discordgo.MessageEmbed{menu},
-			Flags:  discordgo.MessageFlagsEphemeral,
+			Embeds:     []*discordgo.MessageEmbed{menu},
+			Components: buttons,
+			Flags:      discordgo.MessageFlagsEphemeral,
 		},
 	})
 
